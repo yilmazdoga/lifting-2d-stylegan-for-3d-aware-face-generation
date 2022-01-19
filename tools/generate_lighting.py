a8f9b5b6472466f2ce33412c3256ceb1ed72307c
yilmazdoga <yilmaz.doga@gmail.com>
2022-01-20 01:11:50 +0300

    import issue fix

--- tools/generate_lighting.py
+++ tools/generate_lighting.py
@@ -1,3 +1,8 @@
+import sys
+from os import path
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
 import os
 import sys
 import time
@@ -14,7 +19,6 @@ from models.lifted_gan import LiftedGAN
 
 
 def main(args):
-
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
     model = LiftedGAN()
@@ -27,32 +31,33 @@ def main(args):
 
     for head in tqdm(range(0, args.n_samples, args.batch_size)):
         with torch.no_grad():
-            tail = min(args.n_samples, head+args.batch_size)
-            b = tail-head
+            tail = min(args.n_samples, head + args.batch_size)
+            b = tail - head
 
-            latent = torch.randn((b,512))
+            latent = torch.randn((b, 512))
             styles = model.generator.style(latent)
-            styles = args.truncation * styles + (1-args.truncation) * model.w_mu
+            styles = args.truncation * styles + (1 - args.truncation) * model.w_mu
 
-            canon_depth, canon_albedo, canon_light, view, neutral_style, trans_map, canon_im_raw = model.estimate(styles)
+            canon_depth, canon_albedo, canon_light, view, neutral_style, trans_map, canon_im_raw = model.estimate(
+                styles)
 
             recon_relight = []
-            for angle in range(-60,61,6):
+            for angle in range(-60, 61, 6):
                 light = canon_light.clone()
                 angle_ = angle * math.pi / 180
-                light[:,0] = 0.2
-                light[:,1] = 0.8
-                light[:,2] = math.tan(angle_)
-                light[:,3] = 0
+                light[:, 0] = 0.2
+                light[:, 1] = 0.8
+                light[:, 2] = math.tan(angle_)
+                light[:, 3] = 0
                 recon_relight_ = model.render(canon_depth, canon_albedo, light, view, trans_map=trans_map)[0]
                 recon_relight.append(recon_relight_.cpu())
 
-            outputs = torch.stack(recon_relight, 1).clamp(min=-1.,max=1.) # N x M x C x H x W
-            outputs = outputs.permute(0,1,3,4,2).numpy() * 0.5 + 0.5
-            outputs = (outputs*255).astype(np.uint8)
+            outputs = torch.stack(recon_relight, 1).clamp(min=-1., max=1.)  # N x M x C x H x W
+            outputs = outputs.permute(0, 1, 3, 4, 2).numpy() * 0.5 + 0.5
+            outputs = (outputs * 255).astype(np.uint8)
 
             for i in range(outputs.shape[0]):
-                mimwrite(f'{args.output_dir}/{head+i+1}.gif', outputs[i])
+                mimwrite(f'{args.output_dir}/{head + i + 1}.gif', outputs[i])
 
 
 if __name__ == '__main__':
