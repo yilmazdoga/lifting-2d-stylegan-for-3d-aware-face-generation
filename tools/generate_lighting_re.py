c86819c4c402cdc115e564796b45db9750e0439a
yilmazdoga <yilmaz.doga@gmail.com>
2022-01-23 22:12:16 +0300

    added generate_lighting_re.py

--- tools/generate_lighting_re.py
+++ tools/generate_lighting_re.py
@@ -47,33 +47,18 @@ def main(args):
             canon_depth, canon_albedo, canon_light, view, neutral_style, trans_map, canon_im_raw = model_original.estimate(
                 styles)
 
-            recon_rotate = []
-            if args.type == 'yaw':
-                angles = range(-45, 46, 3)
-            elif args.type == 'pitch':
-                angles = range(-15, 16, 1)
-            else:
-                raise ValueError(f'Unkown angle type: {args.type}')
+            recon_relight = []
+            for angle in range(-60, 61, 6):
+                light = canon_light.clone()
+                angle_ = angle * math.pi / 180
+                light[:, 0] = 0.2
+                light[:, 1] = 0.8
+                light[:, 2] = math.tan(angle_)
+                light[:, 3] = 0
+                recon_relight_ = model_original.render(canon_depth, canon_albedo, light, view, trans_map=trans_map)[0]
+                recon_relight.append(recon_relight_.cpu())
 
-            for angle in angles:
-                view_rotate = view.clone()
-                if args.type == 'yaw':
-                    angle_ = -1 * angle / model_original.xyz_rotation_range
-                    view_rotate[:, 0] = torch.ones(b) * 0.0
-                    view_rotate[:, 1] = torch.ones(b) * angle_
-                else:
-                    angle_ = angle / model_original.xyz_rotation_range
-                    view_rotate[:, 0] = torch.ones(b) * angle_
-                    view_rotate[:, 1] = torch.ones(b) * 0
-                view_rotate[:, 2] = torch.ones(b) * 0
-                view_rotate[:, 3] = torch.sin(view_rotate[:, 1]) * 0.1
-                view_rotate[:, 4] = - torch.sin(view_rotate[:, 0]) * 0.2
-                view_rotate[:, 5] = torch.ones(b) * 0
-                rocon_rotate_ = \
-                    model_original.render(canon_depth, canon_albedo, canon_light, view_rotate, trans_map=trans_map)[0]
-                recon_rotate.append(rocon_rotate_.cpu())
-
-            outputs_original = torch.stack(recon_rotate, 1).clamp(min=-1., max=1.)  # N x M x C x H x W
+            outputs_original = torch.stack(recon_relight, 1).clamp(min=-1., max=1.)  # N x M x C x H x W
             outputs_original = outputs_original.permute(0, 1, 3, 4, 2).numpy() * 0.5 + 0.5
             outputs_original = (outputs_original * 255).astype(np.uint8)
 
@@ -88,33 +73,18 @@ def main(args):
             canon_depth, canon_albedo, canon_light, view, neutral_style, trans_map, canon_im_raw = model_reproduced.estimate(
                 styles)
 
-            recon_rotate = []
-            if args.type == 'yaw':
-                angles = range(-45, 46, 3)
-            elif args.type == 'pitch':
-                angles = range(-15, 16, 1)
-            else:
-                raise ValueError(f'Unkown angle type: {args.type}')
+            recon_relight = []
+            for angle in range(-60, 61, 6):
+                light = canon_light.clone()
+                angle_ = angle * math.pi / 180
+                light[:, 0] = 0.2
+                light[:, 1] = 0.8
+                light[:, 2] = math.tan(angle_)
+                light[:, 3] = 0
+                recon_relight_ = model_reproduced.render(canon_depth, canon_albedo, light, view, trans_map=trans_map)[0]
+                recon_relight.append(recon_relight_.cpu())
 
-            for angle in angles:
-                view_rotate = view.clone()
-                if args.type == 'yaw':
-                    angle_ = -1 * angle / model_reproduced.xyz_rotation_range
-                    view_rotate[:, 0] = torch.ones(b) * 0.0
-                    view_rotate[:, 1] = torch.ones(b) * angle_
-                else:
-                    angle_ = angle / model_reproduced.xyz_rotation_range
-                    view_rotate[:, 0] = torch.ones(b) * angle_
-                    view_rotate[:, 1] = torch.ones(b) * 0
-                view_rotate[:, 2] = torch.ones(b) * 0
-                view_rotate[:, 3] = torch.sin(view_rotate[:, 1]) * 0.1
-                view_rotate[:, 4] = - torch.sin(view_rotate[:, 0]) * 0.2
-                view_rotate[:, 5] = torch.ones(b) * 0
-                rocon_rotate_ = \
-                model_reproduced.render(canon_depth, canon_albedo, canon_light, view_rotate, trans_map=trans_map)[0]
-                recon_rotate.append(rocon_rotate_.cpu())
-
-            outputs_reproduced = torch.stack(recon_rotate, 1).clamp(min=-1., max=1.)  # N x M x C x H x W
+            outputs_reproduced = torch.stack(recon_relight, 1).clamp(min=-1., max=1.)  # N x M x C x H x W
             outputs_reproduced = outputs_reproduced.permute(0, 1, 3, 4, 2).numpy() * 0.5 + 0.5
             outputs_reproduced = (outputs_reproduced * 255).astype(np.uint8)
 
@@ -131,12 +101,10 @@ if __name__ == '__main__':
                         type=str)
     parser.add_argument("--output_dir", help="The output path",
                         type=str)
-    parser.add_argument("--type", help="The path to aligned face images",
-                        type=str, default="yaw")
-    parser.add_argument("--truncation", help="Truncation of latent styles",
-                        type=int, default=0.7)
     parser.add_argument("--n_samples", help="Number of images to generate",
                         type=int, default=100)
+    parser.add_argument("--truncation", help="Truncation of latent styles",
+                        type=int, default=0.7)
     parser.add_argument("--batch_size", help="Number of images per mini batch",
                         type=int, default=16)
     args = parser.parse_args()
