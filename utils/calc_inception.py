2ec2745e8e4db43a1ff551581d18f65b04373049
yilmazdoga <yilmaz.doga@gmail.com>
2022-01-31 00:12:04 +0300

    updated fid script

--- utils/calc_inception.py
+++ utils/calc_inception.py
@@ -3,6 +3,12 @@ from os import path
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
+from io import BytesIO
+
+import lmdb
+from PIL import Image
+from torch.utils.data import Dataset
+
 import argparse
 import pickle
 import os
@@ -16,8 +22,43 @@ from torchvision.models import inception_v3, Inception3
 import numpy as np
 from tqdm import tqdm
 
-from dataset import MultiResolutionDataset
 from inception import InceptionV3
+
+
+class MultiResolutionDataset(Dataset):
+    def __init__(self, path, transform, resolution=256):
+        self.env = lmdb.open(
+            path,
+            max_readers=32,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False,
+        )
+
+        if not self.env:
+            raise IOError('Cannot open lmdb dataset', path)
+
+        with self.env.begin(write=False) as txn:
+            self.length = int(txn.get('length'.encode('utf-8')).decode('utf-8'))
+
+        self.resolution = resolution
+        self.transform = transform
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        with self.env.begin(write=False) as txn:
+            key = f'{self.resolution}-{str(index).zfill(5)}'.encode('utf-8')
+            img_bytes = txn.get(key)
+
+        buffer = BytesIO(img_bytes)
+        img = Image.open(buffer)
+        img = self.transform(img)
+
+        return img
+
 
 class Inception3Feature(Inception3):
     def forward(self, x):
