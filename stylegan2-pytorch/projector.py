4ad32aeaf7990b4fc0283125c4eb6bba84717370
yilmazdoga <yilmaz.doga@gmail.com>
2022-02-03 19:38:01 +0300

    import fix

--- stylegan2-pytorch/projector.py
+++ stylegan2-pytorch/projector.py
@@ -14,7 +14,7 @@ from torchvision import transforms
 from PIL import Image
 from tqdm import tqdm
 
-import lpips
+from lpips.__init__ import PerceptualLoss
 from model import Generator
 
 
@@ -163,7 +163,7 @@ if __name__ == "__main__":
         latent_mean = latent_out.mean(0)
         latent_std = ((latent_out - latent_mean).pow(2).sum() / n_mean_latent) ** 0.5
 
-    percept = lpips.PerceptualLoss(
+    percept = PerceptualLoss(
         model="net-lin", net="vgg", use_gpu=device.startswith("cuda")
     )
 
